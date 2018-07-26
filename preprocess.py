32ef24762046d0e41aa14f17eb0a4ee7bce83208
quocpbc <quocpbc@vng.com.vn>
2018-07-26 09:33:13 +0700

    data exploration

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,21 @@
+from multiprocessing import Pool
 import re
 import string
 translator=str.maketrans('','',string.punctuation)
 
 def processline(line):
-    line = line.lower()
+    line = line.strip().lower()
     line=line.translate(translator)
+    line = re.sub('“|”', ' ', line)
+
+    spaceline = line
     line = re.sub(' ', '', line)
-    return line
+    return line +','+ spaceline
 
 if __name__ == '__main__':
     fin = open('data/VNESEcorpus.txt')
     lines = fin.readlines()
-    print(processline(lines[0]))
+    p = Pool(5)
+    lines = p.map(processline, lines)
+    fout = open('data/corpus', 'w')
+    fout.write('\n'.join(lines))
