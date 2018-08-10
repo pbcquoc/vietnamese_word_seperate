25c1bac5d7d037167b8e01e9fa205dab7e72a321
quocpbc <quocpbc@vng.com.vn>
2018-08-10 17:00:12 +0700

    model

--- lstm.py
+++ lstm.py
@@ -32,9 +32,26 @@ def model():
     encoder = LSTM(OUTPUT_DIM, return_state=True)
     encoder_outputs, state_h, state_c = encoder(encoder_inputs)
     encoder_states = [state_h, state_c]
+
     # define training decoder
     decoder_inputs = Input(shape=(None, n_output))
-     
+    decoder_lstm = LSTM(OUTPUT_DIM, return_sequences=True, return_state=True)
+    decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+    decoder_dense = Dense(WORD_VOCAB, activation='softmax')
+    decoder_outputs = decoder_dense(decoder_outputs)
+    model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    # define encoder model
+    encoder_model = Model(encoder_inputs, encoder_states)
+    
+    # define inference decoder
+    decoder_state_input_h = Input(shape=(n_units,))
+    decoder_state_input_c = Input(shape=(n_units,))
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+    decoder_states = [state_h, state_c]
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+
 df = pd.read_csv('../data/word_seperate/corpus', header=None)   
 X, y = df[0].values, df[1].values
 
